c1980660dcded297cc3a9d8fc7d2cefea636dda8
beatriz <beatriz.evago@gmail.com>
2023-04-12 20:24:19 +0100

    desafio3.sql OK

--- challenges/desafio3.sql
+++ challenges/desafio3.sql
@@ -1,12 +1,13 @@
+
 SELECT
-  users.nome_pessoa_usuaria AS pessoa_usuaria,
-  COUNT(historico.cancoes_id) AS musicas_ouvidas,
-  ROUND(SUM(cancoes.duracao_segundos) / 60, 2) AS total_minutos
+  pessoas_usuarias.nome_pessoa_usuaria AS pessoa_usuaria,
+  COUNT(historico.cancao_id) AS musicas_ouvidas,
+  ROUND(SUM(cancoes.duracao_segundos)/60, 2) AS total_minutos
 FROM
-  users
-  JOIN historico ON users.pessoa_usuaria_id = historico.pessoa_usuaria_id
-  JOIN cancoes ON historico.cancoes_id = cancoes.cancoes_id
+  pessoas_usuarias
+    JOIN historico ON pessoas_usuarias.pessoa_usuaria_id = historico.pessoa_usuaria_id
+    JOIN cancoes ON historico.cancao_id = cancoes.cancao_id
 GROUP BY
   pessoa_usuaria
 ORDER BY
-  pessoa_usuaria ASC
+  pessoa_usuaria ASC;
